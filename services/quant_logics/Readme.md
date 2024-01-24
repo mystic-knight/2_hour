77f81f2def26a66d509a0b052821d49bbd08dc47
Saksham573001 <saksham.sharma@cogoport.com>
2024-01-24 17:03:43 +0530

    fix: google auth done

---
+++ services/quant_logics/Readme.md
@@ -0,0 +1,1 @@
+mostly good algo's are private so, all opensource algorithms will have some flaws and will not give nice results
--- services/user/user_router.py
+++ services/user/user_router.py
@@ -10,9 +10,26 @@ from services.user.interactions.get_current_user import get_current_user
 from services.user.interactions.check_user_name import check_user_name
 from services.user.interactions.request_otp import request_otp
 from services.user.models.user import User
+import requests
+from jose import jwt
 
+# should be in a seprate file
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+GOOGLE_CLIENT_ID = "333736231292-mi3e9gkn4br1dgtfuu0raflci7jghlrv.apps.googleusercontent.com"
+GOOGLE_CLIENT_SECRET = "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"
+FRONTEND_URL =  'http://127.0.0.1:8000'
+
+
+config_data = {'GOOGLE_CLIENT_ID': GOOGLE_CLIENT_ID, 'GOOGLE_CLIENT_SECRET': GOOGLE_CLIENT_SECRET}
+import google_auth_oauthlib.flow
+flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+    'test.json',
+    scopes=['https://www.googleapis.com/auth/drive'])
+flow.redirect_uri = FRONTEND_URL
 
 user_router = APIRouter()
+
+
 
 @user_router.post('/register')
 def register_api(request: RegisterUser):
@@ -77,3 +94,32 @@ def send_otp_via_twilio(to_phone_number: str):
 
 
 
+
+@user_router.get("/login/google")
+async def login_google():
+    authorization_url, state = flow.authorization_url(access_type='offline',include_granted_scopes='true')
+    return {
+        "url": authorization_url
+    }
+
+
+@user_router.get("/auth/google")
+async def auth_google(code: str):
+    token_url = "https://accounts.google.com/o/oauth2/token"
+    data = {
+        "code": code,
+        "client_id": GOOGLE_CLIENT_ID,
+        "client_secret": GOOGLE_CLIENT_SECRET,
+        "redirect_uri": FRONTEND_URL,
+        "grant_type": "authorization_code",
+    }
+    response = requests.post(token_url, data=data)
+    access_token = response.json().get("access_token")
+    user_info = requests.get("https://www.googleapis.com/oauth2/v1/userinfo", headers={"Authorization": f"Bearer {access_token}"})
+    return user_info.json()
+
+
+
+@user_router.get("/token")
+async def get_token(token: str = Depends(oauth2_scheme)):
+    return jwt.decode(token, GOOGLE_CLIENT_SECRET, algorithms=["HS256"])
